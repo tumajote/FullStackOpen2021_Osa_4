704799808b194744d1460d37d8c950c6faa23457
tumajote <42946704+tumajote@users.noreply.github.com>
2021-09-06 18:10:17 +0300

    4.1-4.2

---
+++ app.js
@@ -0,0 +1,17 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+
+mongoose.connect(process.env.MONGODB_URI)
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
